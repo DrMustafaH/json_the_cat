4fae2d4c0d5845bf7ab92e4be9d9148922866073
DrMustafaH <mustafaharith@gmail.com>
2021-01-14 16:06:25 -0800

    add test and refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,23 @@
-const myArgs = process.argv.slice(2);
 const request = require('request')
-let catBreedPassed = myArgs[0].substring(0, 4);
 
-request(`https://api.thecatapi.com/v1/images/search?breed_id=${catBreedPassed}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return error;
-  }
-  const bodyObj = JSON.parse(body);
-  if (bodyObj.length === 0) {
-    console.log("Cat breed not found");
-  } else {
-    console.log(bodyObj[0].breeds[0].description);
-  }
-});
+const fetchBreedDescription = function (breedName, callback) {
+  const shortBreedName = breedName.substring(0, 4);
+  request(`https://api.thecatapi.com/v1/images/search?breed_id=${shortBreedName}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+    } else {
+      const bodyObj = JSON.parse(body);
+      if (bodyObj.length === 0) {
+        callback(error, bodyObj.length);
+      } else {
+        const description = bodyObj[0].breeds[0].description;
+        callback(error, description);
+      }
+    }
+  });
+};
+
+
+module.exports = {
+  fetchBreedDescription
+}
